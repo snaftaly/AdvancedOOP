cb7d7bbea3ca8b46d0c6b0c602938d050f5b5249
snaftaly <snaftaly@snaftaly.local>
2016-05-15 23:25:51 +0300

    adding TODO

--- src/Algorithms/_305220980_D.cpp
+++ src/Algorithms/_305220980_D.cpp
@@ -8,3 +8,9 @@ Direction _305220980_D::step(Direction prevStep) {
 void _305220980_D::setSensor(const AbstractSensor& sensor) {
 
 }
+
+// TODO:
+// 1) use amir's code for getting the data of prev step, including how to get back to docking
+// 2) use bfs to get closest un-reveald path (using parent) OR understand amir's code for getting next step.
+// 		but use a different order for searching for each of the algos
+// 3) use our logic for returning back to the house - but remember to use the distance from docking from amir's code
